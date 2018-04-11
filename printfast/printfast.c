9137c4e1da4b1cc72d7cb73dcf3952bada592031
Sinane SI MOHAMMED <ssi-moha@e3r6p14.42.fr>
2018-04-11 11:24:43 +0200

    yoyo

---
+++ printfast/printfast.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printfast.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssi-moha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/20 20:36:42 by ssi-moha          #+#    #+#             */
+/*   Updated: 2018/04/04 14:58:23 by ssi-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printfast.h"
+
+int		get_ret(char *s)
+{
+	int i;
+	t_att option;
+
+	i = 1;
+	option = init_struct();
+	if (s[i] == '-')
+	{
+		i++;
+		option.minus = 1;
+	}
+	printf("%s\n", s + i);
+	option.width = get_width(s, &i);
+	while (is_type(s[i]) < 0)
+		i++;
+	option.type = is_type(s[i]);
+	printf("%d , %d\n", option.width, option.type);
+	return (i + 1);
+}
+
+void	ft_count(char *s, va_list params, int *k)
+{
+	char	buff[BUFF_SIZE + 1];
+	int		i;
+	int		idx_buff;
+	t_att	ret;
+
+	i = 0;
+	idx_buff = 0;
+	while (s[i])
+	{
+		while (s[i] != '%' && s[i])
+		{
+			if (idx_buff == BUFF_SIZE)
+			{
+				write(1, buff, idx_buff);
+				idx_buff = 0;
+				ft_bzero(buff, BUFF_SIZE + 1);
+			}
+			buff[idx_buff++] = s[i++];
+			(*k)++;
+		}
+		if (s[i] == '%')
+		{
+			i += get_ret(s + i);
+		}
+	}
+	write(1, buff, idx_buff);
+}
+
+int		printfast(const char *s, ...)
+{
+	va_list params;
+	int k;
+
+	k = 0;
+	va_start(params, s);
+	ft_count(ft_strdup(s), params, &k);
+	va_end(params);
+	return (k);
+}
+
+int main()
+{
+	printfast("COUCOU%42dlaflute");
+}
---
+++ printfast/printfast.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssi-moha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/04 13:16:03 by ssi-moha          #+#    #+#             */
+/*   Updated: 2018/04/04 14:58:40 by ssi-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTFAST_H
+# define PRINTFAST_H
+# define BUFF_SIZE 10
+# define CONV_PRINT "sdiucl"
+# include "libft/includes/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdarg.h>
+
+typedef struct	s_att
+{
+	int		minus;
+	int			width;
+	short		type;
+	char		*mod;
+	int			len;
+	int			count;
+}				t_att;
+
+int		is_type(char c);
+int		get_width(char *s, int *i);
+t_att	init_struct(void);
+int		printfast(const char *s, ...);
+#endif
